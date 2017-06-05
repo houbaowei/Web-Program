0ba8387ae49c0f8eec2d3844842e97f724ae0f48
zhunjiee <zhunjiee@qq.com>
2017-06-05 22:58:27 +0800

    固定导航栏位置

--- JavaScript/js入门04/js/myScroll.js
+++ JavaScript/js入门04/js/myScroll.js
@@ -1,6 +1,9 @@
 /**
  * Created by ZHUNJIEE on 2017/6/5.
  */
+function $(id) {
+    return document.getElementById(id);
+}
 function scroll() {
     if (window.pageYOffset != null) {   // ie9+ 和 其他浏览器
         return {
@@ -8,6 +11,14 @@ function scroll() {
             top : window.pageYOffset
         }
     } else if (document.compatMode == 'CSS1Compat') {   // 怪异浏览器(未声明<DOCTYPE HTML>)
+        return {
+            left : document.documentElement.scrollLeft,
+            top : document.documentElement.scrollTop
+        }
+    }
 
+    return {
+        left : document.body.scrollLeft,
+        top : document.body.scrollTop
     }
 }
