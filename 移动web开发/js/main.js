149f41373e4b2ad8231349e3e2f5661a6c54522f
zhunjiee <zhunjiee@qq.com>
2017-08-07 08:51:05 +0800

    不知道提交的啥

--- 移动web开发/js/main.js
+++ 移动web开发/js/main.js
@@ -1,4 +1,5 @@
 $(function () {
+    // --- 初始化轮播图插件 ---
     var swiper = new Swiper('.swiper-container', {
         pagination: '.swiper-pagination',
         paginationClickable: true,
@@ -6,4 +7,17 @@ $(function () {
         loop: true, // 循环播放
         autoplayDisableOnInteraction : false, // 滑动后还能自动轮播
     });
+
+    // 首页header透明显示
+    var mainHeader = $('.jd-header .jd-header-box');
+    var bannerH = $('.jd-banner').height();
+
+    $(window).scroll(function () {
+        var scrollH = $(this).scrollTop();
+        if (scrollH > bannerH) {
+            mainHeader.css('backgroundColor', 'rgba(201, 21, 35, 1.0)');
+        } else {
+            mainHeader.css('backgroundColor', 'rgba(201, 21, 35,' + scrollH/bannerH + ')');
+        }
+    })
 });
